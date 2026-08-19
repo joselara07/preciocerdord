531f72db046946741920b01c06b17d027feb4185
joselara07 <59100285+joselara07@users.noreply.github.com>
2026-08-19 16:24:35 -0400

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,51 @@
+const locations = {
+
+    "La Vega": {
+
+        "La Vega": [
+            "Río Verde Arriba",
+            "El Ranchito",
+            "Taveras"
+        ],
+
+        "Constanza": [
+            "Tireo",
+            "La Sabina"
+        ],
+
+        "Jarabacoa": [
+            "Buena Vista",
+            "Manabao"
+        ],
+
+        "Jima Abajo": [
+            "Rincón"
+        ]
+
+    },
+
+
+    "Sánchez Ramírez": {
+
+        "Cotuí": [
+            "Caballero",
+            "Comedero Arriba",
+            "Quita Sueño"
+        ],
+
+        "Cevicos": [
+            "La Cueva",
+            "Platanal"
+        ],
+
+        "Fantino": [],
+
+        "La Mata": [
+            "Angelina",
+            "La Bija",
+            "Hernando Alonzo"
+        ]
+
+    }
+
+};

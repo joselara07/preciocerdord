cabee07f253ea584474f3026c5d5842849c26da8
joselara07 <59100285+joselara07@users.noreply.github.com>
2026-08-20 07:54:57 -0400

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,11 +1,324 @@
 const locations = {
 
+    "Distrito Nacional": {
+
+        "Santo Domingo de Guzmán": []
+
+    },
+
+
+    "Azua": {
+
+        "Azua": [
+            "Barro Arriba",
+            "Las Barías-La Estancia",
+            "Los Jovillos",
+            "Puerto Viejo"
+        ],
+
+        "Las Charcas": [
+            "Palmar de Ocoa"
+        ],
+
+        "Las Yayas de Viajama": [
+            "Villarpando",
+            "Hato Nuevo Cortés"
+        ],
+
+        "Padre Las Casas": [
+            "Las Lagunas",
+            "La Siembra",
+            "Monte Bonito",
+            "Los Fríos"
+        ],
+
+        "Peralta": [],
+
+        "Sabana Yegua": [
+            "Proyecto 4",
+            "Ganadero",
+            "Proyecto 2-C"
+        ],
+
+        "Pueblo Viejo": [
+            "El Rosario"
+        ],
+
+        "Tábara Arriba": [
+            "Tábara Abajo",
+            "Amiama Gómez",
+            "Los Toros"
+        ],
+
+        "Guayabal": [],
+
+        "Estebanía": []
+
+    },
+
+
+    "Baoruco": {
+
+        "Neiba": [
+            "El Palmar"
+        ],
+
+        "Galván": [
+            "El Salado"
+        ],
+
+        "Tamayo": [
+            "Uvilla",
+            "Santana",
+            "Monserrate o Montserrat",
+            "Cabeza de Toro",
+            "Mena",
+            "Santa Bárbara El 6"
+        ],
+
+        "Villa Jaragua": [
+            "Las Clavellinas"
+        ],
+
+        "Los Ríos": []
+
+    },
+
+
+    "Barahona": {
+
+        "Barahona": [
+            "El Cachón",
+            "La Guázara",
+            "Villa Central"
+        ],
+
+        "Cabral": [],
+
+        "Enriquillo": [
+            "Arroyo Dulce"
+        ],
+
+        "Paraíso": [
+            "Los Patos"
+        ],
+
+        "Vicente Noble": [
+            "Canoa",
+            "Quita Coraza",
+            "Fondo Negro"
+        ],
+
+        "El Peñón": [],
+
+        "La Ciénaga": [],
+
+        "Fundación": [
+            "Pescadería"
+        ],
+
+        "Las Salinas": [],
+
+        "Polo": [],
+
+        "Jaquimeyes": []
+
+    },
+
+
+    "Dajabón": {
+
+        "Dajabón": [
+            "Cañongo"
+        ],
+
+        "Loma de Cabrera": [
+            "Capotillo",
+            "Santiago de La Cruz"
+        ],
+
+        "Partido": [
+            "Manuel Bueno"
+        ],
+
+        "Restauración": [],
+
+        "El Pino": []
+
+    },
+
+
+    "Duarte": {
+
+        "San Francisco de Macorís": [
+            "La Peña",
+            "Cenoví",
+            "Jaya",
+            "Presidente Don Antonio Guzmán Fernández"
+        ],
+
+        "Arenoso": [
+            "Las Coles",
+            "El Aguacate"
+        ],
+
+        "Castillo": [],
+
+        "Pimentel": [],
+
+        "Villa Riva": [
+            "Agua Santa del Yuna",
+            "Cristo Rey de Guaraguao",
+            "Las Taranas",
+            "Barraquito"
+        ],
+
+        "Las Guáranas": [
+            "Sabana Grande"
+        ],
+
+        "Eugenio María de Hostos": []
+
+    },
+
+
+    "Elías Piña": {
+
+        "Comendador": [],
+
+        "Bánica": [
+            "Sabana Cruz",
+            "Sabana Higüero"
+        ],
+
+        "El Llano": [
+            "Guanito"
+        ],
+
+        "Hondo Valle": [
+            "Rancho de La Guardia"
+        ],
+
+        "Pedro Santana": [
+            "Río Limpio"
+        ],
+
+        "Juan Santiago": []
+
+    },
+
+
+    "El Seibo": {
+
+        "El Seibo": [
+            "Pedro Sánchez",
+            "San Francisco-Vicentillo",
+            "Santa Lucía"
+        ],
+
+        "Miches": [
+            "El Cedro",
+            "La Gina"
+        ]
+
+    },
+
+
+    "Espaillat": {
+
+        "Moca": [
+            "José Contreras",
+            "Juan López",
+            "Las Lagunas",
+            "Canca la Reyna",
+            "El Higüerito",
+            "Monte de la Jagua",
+            "La Ortega"
+        ],
+
+        "Cayetano Germosén": [],
+
+        "Gaspar Hernández": [
+            "Joba Arriba",
+            "Veragua",
+            "Villa Magante"
+        ],
+
+        "Jamao Al Norte": [
+            "Jamao Afuera"
+        ],
+
+        "San Víctor": [
+            "Blanco"
+        ]
+
+    },
+
+
+    "Independencia": {
+
+        "Jimaní": [
+            "El Limón",
+            "Boca de Cachón"
+        ],
+
+        "Duvergé": [
+            "Vengan a Ver"
+        ],
+
+        "La Descubierta": [],
+
+        "Postrer Río": [],
+
+        "Cristóbal": [
+            "Batey 8"
+        ],
+
+        "Mella": [
+            "La Colonia"
+        ]
+
+    },
+
+
+    "La Altagracia": {
+
+        "Higüey": [
+            "Las Lagunas de Nisibón",
+            "La Otra Banda",
+            "Verón Punta Cana"
+        ],
+
+        "San Rafael del Yuma": [
+            "Boca de Yuma",
+            "Bayahíbe"
+        ]
+
+    },
+
+
+    "La Romana": {
+
+        "La Romana": [
+            "Caleta"
+        ],
+
+        "Guaymate": [],
+
+        "Villa Hermosa": [
+            "Cumayasa"
+        ]
+
+    },
+
+
     "La Vega": {
 
         "La Vega": [
             "Río Verde Arriba",
             "El Ranchito",
-            "Taveras"
+            "Tavera",
+            "Don Juan Rodríguez"
         ],
 
         "Constanza": [
@@ -25,25 +338,479 @@ const locations = {
     },
 
 
-    "Sánchez Ramírez": {
+    "María Trinidad Sánchez": {
 
-        "Cotuí": [
-            "Caballero",
-            "Comedero Arriba",
+        "Nagua": [
+            "San José de Matanzas",
+            "Las Gordas",
+            "Arroyo al Medio"
+        ],
+
+        "Cabrera": [
+            "Arroyo Salado",
+            "La Entrada"
+        ],
+
+        "El Factor": [
+            "El Pozo"
+        ],
+
+        "Río San Juan": []
+
+    },
+
+
+    "Monte Cristi": {
+
+        "Monte Cristi": [
+            "Palo Verde"
+        ],
+
+        "Castañuelas": [],
+
+        "Guayubín": [
+            "Villa Elisa",
+            "Hatillo Palma",
+            "Cana Chapetón"
+        ],
+
+        "Las Matas de Santa Cruz": [],
+
+        "Pepillo Salcedo": [
+            "Santa María"
+        ],
+
+        "Villa Vásquez": []
+
+    },
+
+
+    "Pedernales": {
+
+        "Pedernales": [
+            "José Francisco Peña Gómez"
+        ],
+
+        "Oviedo": [
+            "Juancho"
+        ]
+
+    },
+
+
+    "Peravia": {
+
+        "Baní": [
+            "Villa Fundación",
+            "Sabana Buey",
+            "Paya",
+            "Villa Sombrero",
+            "El Carretón",
+            "Catalina",
+            "El Limonal",
+            "Las Barías"
+        ],
+
+        "Nizao": [
+            "Pizarrete",
+            "Santana"
+        ],
+
+        "Matanzas": []
+
+    },
+
+
+    "Puerto Plata": {
+
+        "Puerto Plata": [
+            "Yásica Arriba",
+            "Maimón"
+        ],
+
+        "Altamira": [
+            "Río Grande"
+        ],
+
+        "Guananico": [],
+
+        "Imbert": [],
+
+        "Los Hidalgos": [
+            "Navas"
+        ],
+
+        "Luperón": [
+            "La Isabela",
+            "Belloso",
+            "El Estrecho de Luperón Omar Bross"
+        ],
+
+        "Sosúa": [
+            "Cabarete",
+            "Sabaneta de Yásica"
+        ],
+
+        "Villa Isabela": [
+            "Estero Hondo",
+            "La Jaiba",
+            "Gualete"
+        ],
+
+        "Villa Montellano": []
+
+    },
+
+
+    "Hermanas Mirabal": {
+
+        "Salcedo": [],
+
+        "Tenares": [
+            "Blanco"
+        ],
+
+        "Villa Tapia": []
+
+    },
+
+
+    "Samaná": {
+
+        "Samaná": [
+            "El Limón",
+            "Arroyo Barril",
+            "Las Galeras"
+        ],
+
+        "Sánchez": [],
+
+        "Las Terrenas": []
+
+    },
+
+
+    "San Cristóbal": {
+
+        "San Cristóbal": [
+            "Hato Damas",
+            "Hatillo"
+        ],
+
+        "Sabana Grande de Palenque": [],
+
+        "Bajos de Haina": [
+            "El Carril",
             "Quita Sueño"
         ],
 
+        "Cambita Garabitos": [
+            "Cambita El Pueblecito"
+        ],
+
+        "Villa Altagracia": [
+            "San José del Puerto",
+            "Medina",
+            "La Cuchilla"
+        ],
+
+        "Yaguate": [
+            "Doña Ana"
+        ],
+
+        "San Gregorio de Nigua": [],
+
+        "Los Cacaos": []
+
+    },
+
+
+    "San Juan": {
+
+        "San Juan": [
+            "Pedro Corto",
+            "Sabaneta",
+            "Sabana Alta",
+            "El Rosario",
+            "Hato del Padre",
+            "Guanito",
+            "La Jagua",
+            "Las Maguanas-Hato Nuevo",
+            "Las Charcas de María Nova",
+            "Las Zanjas"
+        ],
+
+        "Bohechío": [
+            "Arroyo Cano",
+            "Yaque"
+        ],
+
+        "El Cercado": [
+            "Derrumbadero",
+            "Batista"
+        ],
+
+        "Juan de Herrera": [
+            "Jínova"
+        ],
+
+        "Las Matas de Farfán": [
+            "Matayaya",
+            "Carrera de Yeguas"
+        ],
+
+        "Vallejuelo": [
+            "Jorjillo"
+        ]
+
+    },
+
+
+    "San Pedro de Macorís": {
+
+        "San Pedro de Macorís": [],
+
+        "Los Llanos": [
+            "El Puerto",
+            "Gautier"
+        ],
+
+        "Ramón Santana": [],
+
+        "Consuelo": [],
+
+        "Quisqueya": [],
+
+        "Guayacanes": []
+
+    },
+
+
+    "Sánchez Ramírez": {
+
+        "Cotuí": [
+            "Quita Sueño",
+            "Caballero",
+            "Comedero Arriba",
+            "Platanal",
+            "Zambrana Abajo"
+        ],
+
         "Cevicos": [
-            "La Cueva",
-            "Platanal"
+            "La Cueva"
         ],
 
         "Fantino": [],
 
-        "La Mata": [
-            "Angelina",
+        "Villa La Mata": [
             "La Bija",
+            "Angelina",
             "Hernando Alonzo"
+        ]
+
+    },
+
+
+    "Santiago": {
+
+        "Santiago": [
+            "Pedro García",
+            "La Canela",
+            "San Francisco de Jacagua",
+            "Hato del Yaque",
+            "Santiago Oeste"
+        ],
+
+        "Bisonó": [],
+
+        "Jánico": [
+            "Juncalito",
+            "El Caimito"
+        ],
+
+        "Licey al Medio": [
+            "Las Palomas"
+        ],
+
+        "San José de Las Matas": [
+            "El Rubio",
+            "La Cuesta",
+            "Las Placetas"
+        ],
+
+        "Tamboril": [
+            "Canca La Piedra"
+        ],
+
+        "Villa González": [
+            "Palmar Arriba",
+            "El Limón"
+        ],
+
+        "Puñal": [
+            "Guayabal",
+            "Canabacoa"
+        ],
+
+        "Sabana Iglesia": [],
+
+        "Baitoa": []
+
+    },
+
+
+    "Santiago Rodríguez": {
+
+        "San Ignacio de Sabaneta": [
+            "Ámina"
+        ],
+
+        "Villa Los Almácigos": [
+            "Maizal",
+            "Jicomé",
+            "Boca de Mao",
+            "Paradero"
+        ],
+
+        "Monción": [
+            "Jaibón",
+            "La Caya",
+            "Cruce de Guayacanes"
+        ]
+
+    },
+
+
+    "Valverde": {
+
+        "Mao": [
+            "Ámina",
+            "Jaibón o Pueblo Nuevo",
+            "Guatapanal"
+        ],
+
+        "Esperanza": [
+            "Maizal",
+            "Jicomé",
+            "Boca de Mao",
+            "Paradero"
+        ],
+
+        "Laguna Salada": [
+            "Jaibón",
+            "La Caya",
+            "Cruce de Guayacanes"
+        ]
+
+    },
+
+
+    "Monseñor Nouel": {
+
+        "Bonao": [
+            "Sabana del Puerto",
+            "Juma Bejucal",
+            "Arroyo Toro - Masipedro",
+            "Jayaco",
+            "La Salvia - Los Quemados"
+        ],
+
+        "Maimón": [],
+
+        "Piedra Blanca": [
+            "Villa de Sonador",
+            "Juan Adrián"
+        ]
+
+    },
+
+
+    "Monte Plata": {
+
+        "Monte Plata": [
+            "Don Juan",
+            "Chirino",
+            "Boyá"
+        ],
+
+        "Bayaguana": [],
+
+        "Sabana Grande de Boyá": [
+            "Gonzalo",
+            "Majagual"
+        ],
+
+        "Yamasá": [
+            "Los Botados",
+            "Mamá Tingó"
+        ],
+
+        "Peralvillo": []
+
+    },
+
+
+    "Hato Mayor": {
+
+        "Hato Mayor": [
+            "Yerba Buena",
+            "Mata Palacio",
+            "Guayabo Dulce"
+        ],
+
+        "Sabana de La Mar": [
+            "Elupina Cordero de Las Cañitas"
+        ],
+
+        "El Valle": []
+
+    },
+
+
+    "San José de Ocoa": {
+
+        "San José de Ocoa": [
+            "La Ciénaga",
+            "Nizao - Las Auyamas",
+            "El Pinar",
+            "El Naranjal"
+        ],
+
+        "Sabana Larga": [],
+
+        "Rancho Arriba": []
+
+    },
+
+
+    "Santo Domingo": {
+
+        "Santo Domingo Este": [
+            "San Luis"
+        ],
+
+        "Santo Domingo Oeste": [],
+
+        "Santo Domingo Norte": [
+            "La Victoria"
+        ],
+
+        "Boca Chica": [
+            "La Caleta"
+        ],
+
+        "San Antonio de Guerra": [
+            "Hato Viejo"
+        ],
+
+        "Los Alcarrizos": [
+            "Palmarejo-Villa Linda",
+            "Pantoja"
+        ],
+
+        "Pedro Brand": [
+            "La Guáyiga",
+            "La Cuaba"
         ]
 
     }
